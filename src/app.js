24d76db7a94cf35bf9bfc44ccd64bb688c6d9bab
maryamrz402 <136293878+maryamrz402@users.noreply.github.com>
2023-06-30 12:48:54 +0430

    added forcast

--- src/app.js
+++ src/app.js
@@ -17,10 +17,53 @@ function formatDate(timestamp){
     "saturday"];
     let day= days[date.getDay()];
     return` ${day} ${hours}:${minutes}`;
+   }
+   function formatDay(timestamp){
+      let date = new Date(timestamp * 1000);
+      let day = date.getDay();
+      let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+      return days[day];
+   }
+function displayForecast(response){
+   let forecast= response.data.daily;
+   let forecastElement = document.querySelector("#forecast");
+   
+   let forecastHTML = `<div class="row">`;
 
-
+   forecast.forEach(function(forecastDay, index){
+   if(index<6){
+       forecastHTML = 
+   forecastHTML+
+   ` <div class="col-2">
+                <div class="weather-forecast-date">${formatDay(forecastDay.time)}</div>
+                
+                <img 
+                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecastDay.condition.icon}.png"; 
+                alt=""
+                width="42"/>
+                <div class="weather-forecast-temperatures">
+                    <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temperature.maximum)}°</span>
+                    <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temperature.minimum)}°</span>
+                
+                
+            </div>
+        </div>
+        `;
+      }
+   });
+        forecastHTML =  forecastHTML + `</div>`;
+        forecastElement.innerHTML = forecastHTML;
+        
+   }
+   function getForecast(coordinates) {
+      
+      let apiKey = "520fod794c0bd8f40170aa28e33at72e";
+      let apiUrl=`https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
+   
+      return axios.get(apiUrl).then(displayForecast);
+   }
  
-    }
+    
 function displayWeather(response){
  
  let countryElemnt=document.querySelector("#country");
@@ -44,6 +87,7 @@ windElement.innerHTML=Math.round( response.data.wind.speed);
 dateElement.innerHTML= formatDate(response.data.time *1000);
 iconElement.setAttribute("src",`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
 iconElement.setAttribute("alt", response.data.condition.description);
+getForecast(response.data.coordinates);
  
  }
  
@@ -96,3 +140,4 @@ let celsiusTemp = null;
 
  let celsiusLink=document.querySelector("#celsius-link");
  celsiusLink.addEventListener("click",showCelsiusTemp);
+ 
